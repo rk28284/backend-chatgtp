b7644e0653ee1fff24d551462bc27d91f568337b
rk28284 <112754760+rk28284@users.noreply.github.com>
2023-09-14 09:57:26 +0530

    Backend chat-gtp

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.get("/",(req,res)=>{
-  res.send("Welcome to homepage of Doctores Backend")
+  res.send("Welcome to homepage of Shyari-Chatgtp Backend")
 })
 app.get('/shayari', async (req, res) => {
   try {
